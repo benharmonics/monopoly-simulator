f353548481ebaad7d36587a1788a2a5b54213b4c
benharmonics <benjamin.jiron+git@gmail.com>
2025-09-17 14:26:22 -0600

    chance cards - initial commit

---
+++ src/player.py
@@ -0,0 +1,10 @@
+from dataclasses import dataclass
+
+@dataclass
+class Player:
+    id: int
+    money: int = 1500
+    space: int = 0
+    jail_sentence: int = 0
+    get_out_of_jail_cards: int = 0
+
